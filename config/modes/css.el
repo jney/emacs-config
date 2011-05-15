cc32fff6b0d36d109fbeb9e55107dfde5064fc0c
Jean-Sebastien Ney <jeansebastien.ney@gmail.com>
2011-05-16 00:39:41 +0200

    check if executables exists

--- config/modes/css.el
+++ config/modes/css.el
@@ -3,7 +3,7 @@
              (set-fill-column 79)
              (setq css-indent-offset 2)
              (add-hook 'after-save-hook
-                       (lambda () (css-check)) nil t)
+                       (lambda () (when (executable-find "csstidy") (css-check))) nil t)
              (autopair-mode 1)
              (rainbow-mode 1)))
 
@@ -11,17 +11,18 @@
 (add-hook 'css-mode-hook 'esk-paredit-nonlisp)
 
 ;; pip install cssutils
-(defun flymake-css-init ()
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                         'flymake-create-temp-inplace))
-             (local-file (file-relative-name
-                          temp-file
-                          (file-name-directory buffer-file-name))))
-        (list "cssparse" (list local-file))))
-    (add-to-list 'flymake-allowed-file-name-masks
-                 '("\\.css$" flymake-css-init))
-    (add-to-list 'flymake-err-line-patterns
-                 '("\\(.*\\) \\[\\([0-9]+\\):\\([0-9]+\\): \\(.*\\)\\]"
-                   nil 2 3 1))
-    (add-hook 'css-mode-hook
-              (lambda () (flymake-mode t)))
+(when (executable-find "cssparse")
+  (defun flymake-css-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "cssparse" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.css$" flymake-css-init))
+  (add-to-list 'flymake-err-line-patterns
+               '("\\(.*\\) \\[\\([0-9]+\\):\\([0-9]+\\): \\(.*\\)\\]"
+                 nil 2 3 1))
+  (add-hook 'css-mode-hook
+            (lambda () (flymake-mode t))))
